b0b308dd6a15ceef3e85c3709fd0124f526ae9a0
yassine-el-ouali2 <yassine.el.ouali2@ibm.com>
2024-04-09 14:30:10 +0000

    Commit design adjustments

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../services/storage/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+
+  constructor(protected storageServ: StorageService,
+    private router: Router) { }
+
+
+
+  ngOnInit() {
+    if (!this.storageServ.isLoggedIn()) {
+      this.router.navigateByUrl("login");
+    }
+  }
 }
